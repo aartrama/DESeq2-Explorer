e27487c6e08c4954c83ebd433858f8f080f47bf5
Aarthi <aarthi@aarthis-air.myfiosgateway.com>
2021-08-29 09:50:20 -0400

    first commit

---
+++ helper_functions.R
@@ -0,0 +1,170 @@
+GetAnnotationFromBiomart <- function(species) {
+  if (species == "hg38") {
+    dataset="hsapiens_gene_ensembl"
+  } else if (species == "mm10") {
+    dataset="mmusculus_gene_ensembl"  
+  } else if (species == "rn6") {
+    dataset="rnorvegicus_gene_ensembl"
+  }
+  mart <- useMart(biomart="ENSEMBL_MART_ENSEMBL", dataset=dataset, host="aug2017.archive.ensembl.org")
+  annotation_biomart <- getBM(attributes = c("ensembl_gene_id","external_gene_name","chromosome_name","start_position", 
+                                             "end_position", "gene_biotype", "description"), 
+                              filters = "ensembl_gene_id", values = rownames(dds), mart = mart) 
+  rownames(annotation_biomart) <- annotation_biomart$ensembl_gene_id
+  annotation_biomart <- subset(annotation_biomart, select=c(-`ensembl_gene_id`))
+  return(annotation_biomart)
+}
+
+AnnotateDifflist <- function(annotations, difflist) {
+  annotations <- annotations[rownames(difflist), ]
+  print(all(rownames(annotations) == rownames(difflist)))
+  annot_difflist <- cbind(difflist, annotations)
+  annot_difflist <- subset(annot_difflist, select=c(7,1,2,3,4,5,6,8,9,10,11,12))
+  colnames(annot_difflist) <- c("gene_name", "baseMean", "log2FoldChange", "lfcSE", "stat", "pvalue", 
+                                "padj", "chromosome", "gene_start",
+                                "gene_end", "gene_type", "description")
+  return(annot_difflist)
+}
+
+ExtractDifferentialListsPairwise <- function(filename, groupname, group2, group1) {
+  difflist <- results(dds, contrast=c(groupname, group2, group1))
+  difflist <- AnnotateDifflist(annotation, difflist) 
+  difflist <- difflist[order(difflist$pvalue), ]
+  difflist$gene_id <- rownames(difflist)
+  write.table(difflist, file = filename, sep = "\t", quote = F, row.names=F)
+}
+
+ExtractDifferentialListsPairwiseExcel <- function(filename, groupname, group2, group1) {
+  difflist <- results(dds, contrast=c(groupname, group2, group1))
+  difflist <- AnnotateDifflist(annotation, difflist) 
+  difflist <- difflist[order(difflist$pvalue), ]
+  difflist$gene_id <- rownames(difflist)
+  write.xlsx(difflist, filename)
+}
+
+ExtractDifferentialListsMultiFactor <- function(filename, var1, var2=NULL) {
+  if (is.null(var2)) {
+    difflist <- results(dds, name=var1)
+    difflist <- AnnotateDifflist(annotation, difflist) 
+    difflist <- difflist[order(difflist$pvalue), ]
+    difflist$gene_id <- rownames(difflist)
+    write.table(difflist, file = filename, sep = "\t", quote = F, row.names=F)
+    return(difflist)
+    
+  } else {
+    difflist <- results(dds, contrast = list(c(var1, var2)))
+    difflist <- AnnotateDifflist(annotation, difflist) 
+    difflist <- difflist[order(difflist$pvalue), ]
+    difflist$gene_id <- rownames(difflist)
+    write.table(difflist, file = filename, sep = "\t", quote = F, row.names=F)
+    return(difflist)
+  }
+}
+
+ExtractDifferentialListsMultiFactorExcel <- function(filename, var1, var2=NULL) {
+  if (is.null(var2)) {
+    difflist <- results(dds, name=var1)
+    difflist <- AnnotateDifflist(annotation, difflist) 
+    difflist <- difflist[order(difflist$pvalue), ]
+    difflist$gene_id <- rownames(difflist)
+    write.xlsx(difflist, filename)
+    
+  } else {
+    difflist <- results(dds, contrast = list(c(var1, var2)))
+    difflist <- AnnotateDifflist(annotation, difflist) 
+    difflist <- difflist[order(difflist$pvalue), ]
+    difflist$gene_id <- rownames(difflist)
+    write.xlsx(difflist, filename)
+  }
+}
+
+GeneratePCA <- function(filename, group1, group2=NULL) {
+  if (is.null(group2)) {
+    pcaData <- plotPCA(vsd.fast, intgroup=c(group1), returnData=TRUE)
+  } else {
+    pcaData <- plotPCA(vsd.fast, intgroup=c(group1, group2), returnData=TRUE)
+  }
+  
+  percentVar <- round(100 * attr(pcaData, "percentVar"))
+  
+  if (is.null(group2)) {
+    plot1 <- ggplot(pcaData, aes(PC1, PC2, color=!!ensym(group1)))
+  } else {
+    plot1 <- ggplot(pcaData, aes(PC1, PC2, color=!!ensym(group1), shape=!!ensym(group2))) 
+  }
+  pdf = pdf(filename)
+  plot1 <- plot1 + geom_point(size=3) + geom_text(aes(label = name), color="black", size = 2, 
+                                                  position = position_nudge(y = 0.2))+
+    xlab(paste0("PC1: ",percentVar[1],"% variance")) +
+    ylab(paste0("PC2: ",percentVar[2],"% variance")) + 
+    coord_fixed()
+  print(plot1)
+  dev.off()
+  
+}
+
+ObtainNormalizedCounts <- function() {
+  annotation <- GetAnnotationFromBiomart(species)
+  data_normalized = counts(dds, normalized = T)
+  annotation <- annotation[rownames(data_normalized), ]
+  all(rownames(annotation) == rownames(data_normalized))
+  data_normalized <- cbind(Gene.name=annotation$external_gene_name, data_normalized)
+  write.table(data_normalized, file = "normalized_expression.txt", sep = "\t", quote = F, col.names = NA)
+  return(data.frame(data_normalized))
+}
+
+ObtainRPKMs <- function(species) {
+  annotation <- GetAnnotationFromBiomart(species)
+  # RPKM =   numReads / ( geneLength/1000 * totalNumReads/1,000,000 )
+  annotation$gene_length <- annotation$end_position - annotation$start_position + 1 
+  # this is how FeatureCounts calculates gene lengths
+  annotation <- subset(annotation, select=c("gene_length", "external_gene_name"))
+  mcols(dds)$basepairs <- annotation$gene_length
+  fpkm_values <- fpkm(dds, robust=F) # robust = F: use colsums for lib size norm 
+  # (as opposed to size factors calculated by DESEQ2)
+  fpkm_values <- as.data.frame(fpkm_values) 
+  Gene.name <- annotation$external_gene_name
+  fpkm_values <- cbind(Gene.name=annotation$external_gene_name, fpkm_values)
+  write.table(fpkm_values, file = "RPKM_values.txt", sep = "\t", quote = F, col.names = NA)
+}
+
+ObtainPvalueLogfcDESeq2 <- function(difflist) {
+  compname <- strsplit(difflist, ".txt")
+  data1 <- read.delim(difflist, sep = "\t", header =T, stringsAsFactors = F)
+  rownames(data1) <- paste0(data1$gene_name, ",", data1$gene_id)
+  data1.subset <- cbind(data1$log2FoldChange, data1$pvalue, data1$padj)
+  rownames(data1.subset) <- rownames(data1)
+  colnames(data1.subset) <- c("log2FoldChange", "pvalue", "padj")
+  colnames(data1.subset) <- c(paste0(compname, ".log2FoldChange"), paste0(compname, ".pvalue"), paste0(compname, ".padj"))
+  data1.subset
+}
+
+RunMultiFactor <- function(WT_list, KO_list, interaction_list, outfile_tsv, pval_or_padj, final_outfile_xlsx) {
+
+    use_python("/usr/bin/python3", required=T)
+    py_config()
+    source_python("helper_functions.py")
+
+    list_of_files = c(WT_list, KO_list, interaction_list)
+
+    result <- lapply(list_of_files, ObtainPvalueLogfcDESeq2)
+
+    intersect_genes <- Reduce(intersect, list(rownames(result[[1]]),
+                                              rownames(result[[2]]),
+                                              rownames(result[[3]])))
+
+    result[[1]] <- result[[1]][intersect_genes,]
+    result[[2]] <- result[[2]][intersect_genes,]
+    result[[3]] <- result[[3]][intersect_genes,]
+
+    table(row.names(result[[1]])==row.names(result[[2]]))
+    table(row.names(result[[2]])==row.names(result[[3]]))
+
+    result_last <- do.call(cbind, result)
+
+    write.table(result_last, file = outfile_tsv, sep = "\t", quote = F, col.names = NA)
+
+    parse_diff_list_table(outfile_tsv, pval_or_padj) # "pvalue" or "padj"
+    integrate_up_down_csv_files_into_excel(final_outfile_xlsx)
+
+}
---
+++ helper_functions.py
@@ -0,0 +1,85 @@
+import csv, xlwt
+
+def integrate_up_down_csv_files_into_excel(outfilename):
+    wb = xlwt.Workbook()
+
+    filenames = ['up_up.csv', 'up_down.csv', 'down_up.csv', 'down_down.csv']
+
+    for filename in filenames:
+        f_short_name = filename.split(".csv")[0]
+        ws = wb.add_sheet(f_short_name.title())
+        data_for_excel = csv.reader(open(filename, "r"))
+        for rowx, row in enumerate(data_for_excel):
+            for colx, value in enumerate(row):
+                ws.write(rowx, colx, value)
+                
+    wb.save(outfilename)
+
+
+def parse_diff_list_table(filename, which_p_value):
+    """
+    This script is for parsing 
+    diff_list_table_lucy.tsv
+    to get a better understanding 
+    of 'interaction' and 
+    multifactor design analysis.
+    """
+    type_of_genes = {"up_up": [], \
+                    "down_down": [], \
+                    "down_up": [], \
+                    "up_down": []}
+
+    header = " "
+
+    with open(filename, "r") as f:
+        for lines in f:
+            lines = lines.strip().split("\t")
+            header = lines
+            break
+
+    with open(filename, "r") as f:
+        next(f)
+        for lines in f:
+            lines = lines.strip().split("\t")
+
+            gene = lines[0].split(",")[0]
+
+            logfc1 = 0 if lines[1] == "NA" else float(lines[1])
+            logfc2 = 0 if lines[4]  == "NA" else float(lines[4])
+            interaction_fc = 0 if lines[7] == "NA" else float(lines[7])
+
+            if which_p_value == "pvalue":
+                pvalue1 = 1 if lines[2] == "NA" else float(lines[2])
+                pvalue2 = 1 if lines[5] == "NA" else float(lines[5])
+                interaction_pvalue = 1 if lines[5] == "NA" else float(lines[8])
+                newfile_header = ",".join(["GeneName", header[0], header[1], header[3], \
+                                            header[4], header[6], header[7]])
+
+            elif which_p_value == "padj":
+
+                pvalue1 = 1 if lines[3] == "NA" else float(lines[3])
+                pvalue2 = 1 if lines[6] == "NA" else float(lines[6])
+                interaction_pvalue = 1 if lines[9] == "NA" else float(lines[9])
+                newfile_header = ",".join(["GeneName", header[0], header[2], header[3], \
+                                            header[5], header[6], header[8]])   
+
+            if interaction_pvalue < 0.05:
+                list_of_values = [gene, logfc1, pvalue1, logfc2, pvalue2, interaction_fc, interaction_pvalue]
+
+                if logfc1 >= 0.0 and interaction_fc >= 0.0:
+                    type_of_genes["up_up"].append(map(str, list_of_values))
+
+                elif logfc1 < 0.0 and interaction_fc < 0.0:
+                    type_of_genes["down_down"].append(map(str, list_of_values))
+
+                elif logfc1 < 0.0 and interaction_fc >= 0.0:
+                    type_of_genes["down_up"].append(map(str, list_of_values))
+
+                elif logfc1 >= 0.0 and interaction_fc < 0.0:
+                    type_of_genes["up_down"].append(map(str, list_of_values))
+
+    for key in type_of_genes:
+        with open(key+".csv", "w") as outfile:
+            outfile.write(newfile_header+"\n")
+            for gene in type_of_genes[key]: 
+                outfile.write(",".join(gene)+"\n")
